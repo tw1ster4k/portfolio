6b090b14291a6e50688e1fcff7f796adbdc031ff
tw1ster4k <aminurshinov@gmail.com>
2026-03-06 12:35:50 +0500

    release1.1.0

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { cn } from '@bem-react/classname';
+import Human from "./images/Human.png";
 
 function App() {
   const navigation = ['Главная','Обо мне','Достижения','Проекты','Контакты']
@@ -13,8 +14,19 @@ function App() {
                     {el}
                  </li> 
                 )}
+                <li>
+                  RU 🇷🇺
+                </li>
                 </ul>
         </header>
+        <div className={cnApp("Main")}>
+          <div className={cnApp("Firts")}>
+                <h1 className={cnApp("Name",{pos:"up"})} >Нуршинов</h1>
+                <h1 className={cnApp("Name",{pos:"down"})} >Амин</h1>
+                <h2 className={cnApp("Description")}>Я просто человек, который сделает вашу жизнь легче</h2>
+                <img className={cnApp("Image")} src={Human} />
+          </div>
+        </div>
     </div>
   );
 }
